3402c4d7ef32e53470af6bf286eecb654f5ef3f1
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2025-10-02 02:27:10 +0000

    create kmni logo

---
+++ src/components/kmni-logo.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+
+export default function Logo() {
+  return (
+    <div className="flex items-center gap-2">
+      {/* Logo Image - Reduced */}
+      <div className="flex-shrink-0">
+        <Image
+          src="/logo.png"
+          alt="KMNI Logo"
+          height={35}
+          width={35}
+          className="object-contain"
+        />
+      </div>
+      
+      {/* Vertical Divider Line - Shorter */}
+      <div className="h-7 w-[1px] bg-green-600"></div>
+      
+      {/* Church Name - Smaller Text */}
+      <div>
+        <h1 className="text-green-700 font-medium text-sm leading-tight">
+          KINGDOM MISSION<br />
+          NETWORK INT.
+        </h1>
+      </div>
+    </div>
+  )
+}
